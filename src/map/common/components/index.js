8340ea46a656b53648ceb40d60e32cc9d9c680b5
wkgreat <wkgreat@outlook.com>
2020-02-05 11:42:13 +0800

    delete unused

--- src/map/common/components/index.js
+++ src/map/common/components/index.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import styled from "styled-components";
 
 const id = "map-div";
 
-const MapDiv = styled.div`
-    height: 100%;
-    background-color: #040508;
-`;
+// const MapDiv = styled.div`
+//     height: 100%;
+//     background-color: #040508;
+// `;
 
 class Olmap extends Component {
 
